e517223fac53393489cb5ddd205180481240a02c
Sander Arjan Benoist <sabenoist5@gmail.com>
2021-09-25 13:10:22 +0200

    added functionality to visualize random amplitudes with a button

---
+++ Assets/Scripts/SoundVisualizer/RandomSoundButton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+/// <summary>
+/// Button to generate random amplitudes to test visualization by the Visualizer3D script.
+/// </summary>
+public class RandomSoundButton : MonoBehaviour
+{
+    /// <summary>
+    /// Reference to the Visualizer3D script.
+    /// </summary>
+    [SerializeField]
+    private Visualizer3D visualizerScript;
+
+    /// <summary>
+    /// The maximum value that can be randomly generated.
+    /// </summary>
+    [SerializeField]
+    private float maxRange;
+
+    /// <summary>
+    /// Generates random values in range of the <see cref="maxRange"/> for the 3d bars of the <see cref="Visualizer3D"/> object.
+    /// </summary>
+    public void GenerateRandomValues() {
+        visualizerScript.SetBarHeight(Microphone.MIC0, Random.Range(0f, maxRange));
+        visualizerScript.SetBarHeight(Microphone.MIC1, Random.Range(0f, maxRange));
+        visualizerScript.SetBarHeight(Microphone.MIC2, Random.Range(0f, maxRange));
+        visualizerScript.SetBarHeight(Microphone.MIC3, Random.Range(0f, maxRange));
+    }
+}
--- Assets/Scripts/SoundVisualizer/Visualizer3D.cs
+++ Assets/Scripts/SoundVisualizer/Visualizer3D.cs
@@ -1,26 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
+/// <summary>
+/// Enum for the different microphones.
+/// </summary>
+public enum Microphone { MIC0, MIC1, MIC2, MIC3 }
+
+/// <summary>
+/// Class to handle the visualization of the 3d bars.
+/// </summary>
 public class Visualizer3D : MonoBehaviour
 {
-	public float spectrumData = 15.0f;//data need to be received 
-    public GameObject visualizerObject;
-	
+	/// <summary>
+	/// Reference to the game object representing the first 3d bar.
+	/// </summary>
+	[SerializeField]
+    private GameObject Bar0;
 
-	// Use this for initialization
-	void Start()
-	{
-		
-	}
+	/// <summary>
+	/// Reference to the game object representing the second 3d bar.
+	/// </summary>
+	[SerializeField]
+	private GameObject Bar1;
 
-	// Update is called once per frame
-	void FixedUpdate()
+	/// <summary>
+	/// Reference to the game object representing the third 3d bar.
+	/// </summary>
+	[SerializeField]
+	private GameObject Bar2;
+
+	/// <summary>
+	/// Reference to the game object representing the fourth 3d bar.
+	/// </summary>
+	[SerializeField]
+	private GameObject Bar3;
+
+	/// <summary>
+	/// Sets the respective 3d bar to the new given height.
+	/// </summary>
+	/// <param name="mic">Enum value of the microphone to determine which bar to change.</param>
+	/// <param name="amplitude">The amplitude value to be determine the height of the bar.</param>
+	public void SetBarHeight(Microphone mic, float amplitude) 
 	{
-			Vector3 newSize = visualizerObject.GetComponent<Transform>().localScale;		
-			newSize.y =  spectrumData;
-			visualizerObject.GetComponent<Transform>().localScale = newSize;
-		
+		GameObject visualizerObject;
+
+		switch (mic) 
+		{
+			case Microphone.MIC0:
+				visualizerObject = Bar0;
+				break;
+			case Microphone.MIC1:
+				visualizerObject = Bar1;
+				break;
+			case Microphone.MIC2:
+				visualizerObject = Bar2;
+				break;
+			case Microphone.MIC3:
+				visualizerObject = Bar3;
+				break;
+			default:
+				return;
+		}
+
+		Vector3 newSize = visualizerObject.GetComponent<Transform>().localScale;
+		newSize.y = amplitude;
+		visualizerObject.GetComponent<Transform>().localScale = newSize;
 	}
 }
