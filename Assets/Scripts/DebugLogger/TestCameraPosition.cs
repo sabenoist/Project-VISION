800cd785020f29c6723c56271bebb4fb2944f4ed
Sander Arjan Benoist <sabenoist5@gmail.com>
2021-11-22 14:37:36 +0100

    testing camera positioning

---
+++ Assets/Scripts/DebugLogger/TestCameraPosition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCameraPosition : MonoBehaviour
+{
+    public Transform transform;
+    public Text text;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = transform.position.x.ToString();
+    }
+}
--- Assets/Scripts/SoundDirectionIndicator/SoundDirectionIndicatorSystem.cs
+++ Assets/Scripts/SoundDirectionIndicator/SoundDirectionIndicatorSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class SoundDirectionIndicatorSystem : MonoBehaviour
 {
